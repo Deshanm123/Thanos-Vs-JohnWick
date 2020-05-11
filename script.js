a4372dc755a55480ff9f7b5f471ef400ca8d2518
Deshanm123 <deshanm123@gmail.com>
2020-05-11 23:25:07 +0530

    add css glow using js DOM Manupulation

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const rockDiv=document.getElementById("r");
 const paperDiv=document.getElementById("p");
 const scissorDiv=document.getElementById("s");
 
-
+//display picked up weapon
 function letterToWeapon(letter){
     if(letter === "p") 
     return "Paper";
@@ -20,19 +20,29 @@ function letterToWeapon(letter){
     else
     return "Scissor";
 }
-
+// Win  display effect
 function win(thanos_input,wick_input){
+ //score display
  userScore++;
  userScoreSpan.innerHTML = userScore;
+ //result status
  matchstatus.innerHTML = `Wick Wins😁.<br>
  Thanos picked ${letterToWeapon(thanos_input)} 🤠 wick picked ${letterToWeapon(wick_input)}`;
+ //adding glow to user clicked choice()
+  document.getElementById(wick_input).classList.add('green-glow'); 
+ //here we take user selected id then go its class list(which get displayed as array of class) ,appended to greenglow class to classes  array
+setTimeout(()=>document.getElementById(wick_input).classList.remove('green-glow'),300);
 }
+
+//Lost Display effect
 function lost(thanos_input,wick_input){
  computerScore++;
  computerScoreSpan.innerHTML = computerScore;
  matchstatus.innerHTML=`Wick loses🤕.<br>Thanos Picked ${letterToWeapon(thanos_input)} 🤠 Wick picked  ${letterToWeapon(wick_input)}`;
+ document.getElementById(wick_input).classList.add('red-glow');
+ setTimeout(()=>document.getElementById(wick_input).classList.remove('red-glow'),300);
 }
-
+//generating random inputs
 function getComputerChoice(){
     const choices = ['r','p','s'];
     const randomNum =(Math.floor(Math.random()*3));
@@ -55,8 +65,11 @@ switch(thanos_input+wick_input){
     case "rr":
     case "ss":
     case "pp":
+        {
         matchstatus.innerHTML=`Thanos Escaped😱.<br> Thanos Picked ${letterToWeapon(thanos_input)} 🤠 Wick picked ${letterToWeapon(wick_input)}`;
-        break;
+        document.getElementById(wick_input).classList.add('gray-glow');
+        setTimeout(()=>document.getElementById(wick_input).classList.remove('gray-glow'),300);
+    }break;
 }
 }
 
